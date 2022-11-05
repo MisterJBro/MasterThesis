b030a2ea6cdf14884bc67ea025a38cb51b43459d
Julien <MisterJBro@protonmail.com>
2022-11-05 01:57:41 +0100

    Changes to tune

--- src/scripts/tune.py
+++ src/scripts/tune.py
@@ -1,5 +1,6 @@
 from hyperopt import hp, tpe, fmin, Trials
 import torch
+import numpy as np
 from multiprocessing import freeze_support
 from src.env.hex import HexEnv
 from src.networks.residual import HexPolicy
@@ -9,6 +10,8 @@ from src.train.pg import PGTrainer, PPOTrainer
 
 # Objective
 def objective(params):
+    print(f"Testing out params: {params}")
+    
     # Init for algos
     size = 9
     env = HexEnv(size)
@@ -18,11 +21,10 @@ def objective(params):
         "env": env,
         "num_cpus": 15,
         "num_envs": 120,
-        "sample_len": 1_000,
         "device": "cuda:0",
-        "self_play_num_eval_games": 100,
-        "num_batch_split": 30,
-        "grad_clip": 1000.0,
+        "self_play_num_eval_games": 240,
+        "num_batch_split": 40,
+        "grad_clip": 100.0,
         "log_to_writer": False,
         "log_to_file": True,
     }
@@ -34,26 +36,34 @@ def objective(params):
     
     with PPOTrainer(config, policy) as tr:
         tr.train()
-        max_win_rate = tr.log.best_metric
+        win_rates = np.array(tr.log.main_metric)
+        
+    # Get netrics
+    max_wr = np.max(win_rates)
+    x = np.arange(len(win_rates))
+    A = np.vstack([x, np.ones(len(x))]).T
+    slope_wr, _ = np.linalg.lstsq(A, win_rates, rcond=None)[0]
+    max_drop_wr = np.max(win_rates[:-1] - win_rates[1:])
 
     # Write progress
     f = open("tune_log.txt", "a")
-    f.write(f'Max Winrate: {max_win_rate}:\nParams: {params}\n')
+    f.write(f'Max Winrate: {np.max(win_rates)}  Slope: {slope_wr}  Max Drop: {max_drop_wr}\nParams: {params}\n')
     f.close()
 
-    return -max_win_rate
+    return -(max_wr - 0.5*max_drop_wr) * (1 if slope_wr > 0 else 0)
 
 # Space
 space = {
-    "pi_lr": hp.choice("pi_lr", [1e-1, 3e-2, 2e-2, 1e-2, 6e-3, 3e-3, 1e-3, 6e-4, 1e-4, 1e-5]),
-    "pi_entropy": hp.uniform("pi_entropy", 0.0, 0.05),
-    "clip_ratio": hp.uniform("clip_ratio", 0.05, 0.3),
+    "pi_lr": hp.choice("pi_lr", [4e-3, 3e-3, 1e-3, 6e-4, 3e-4, 1e-4, 6e-5]),
+    "pi_entropy": hp.uniform("pi_entropy", 0.01, 0.05),
+    "clip_ratio": hp.uniform("clip_ratio", 0.1, 0.3),
     "num_filters": hp.choice("num_filters", [64, 128, 192, 256]),
     "num_res_blocks": hp.choice("num_res_blocks", [10, 12, 16, 18, 22, 24]),
     "use_se": hp.choice("use_se", [False, True]),
     "ppo_iters": hp.randint("ppo_iters", 58) + 2,
-    "vf_scale": hp.uniform("vf_scale", 0.1, 2.0),
-    "kl_approx_max": hp.uniform("kl_approx_max", 0.01, 2.0),
+    "vf_scale": hp.uniform("vf_scale", 0.4, 2.0),
+    "kl_approx_max": hp.uniform("kl_approx_max", 0.2, 2.0),
+    "sample_len": hp.choice("sample_len", [1000, 2000, 4000, 8000]),
 }
 
 if __name__ == '__main__':
