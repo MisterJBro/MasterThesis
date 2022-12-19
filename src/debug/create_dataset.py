9c04dcc16dff54d42143449e64c283dee6d3f347
Julien B <MisterJBro@protonmail.com>
2022-12-19 23:47:29 +0100

    Dataset with epsilon greedy

--- src/debug/create_dataset.py
+++ src/debug/create_dataset.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     freeze_support()
 
     # Init for algos
-    size = 5
+    size = 7
     env = HexEnv(size)
     config = create_config({
         "env": env,
@@ -36,7 +36,7 @@ if __name__ == '__main__':
         "device": "cpu",
         "search_return_adv": True,
 
-        "num_res_blocks": 10,
+        "num_res_blocks": 16,
         "num_filters": 128,
         "model_num_res_blocks": 10,
         "model_num_filters": 128,
@@ -44,7 +44,7 @@ if __name__ == '__main__':
 
     # Import policy and model
     policy1 = HexPolicy(config)
-    policy1.load("checkpoints/p_53_elo_1026.0.pt")
+    policy1.load("hall_of_fame/p_7x7_16_128.pt")
     policy1.eval()
     az = AlphaZero(config, policy1)
 
@@ -65,7 +65,16 @@ if __name__ == '__main__':
             black_turn = True
             while not done:
                 states.append(State(deepcopy(env), obs=np.array(obs, copy=True)))
-                act = random.choice(np.arange(size**2)[info["legal_act"]])
+
+                # Random action 90%, policy inference 10%
+                if random.random() < 0.9:
+                    act = random.choice(np.arange(size**2)[info["legal_act"]])
+                else:
+                    obs_tensor = torch.as_tensor(obs, dtype=torch.float32).unsqueeze(0).to(policy1.device)
+                    with torch.no_grad():
+                        dist_pol, _ = policy1(obs_tensor, legal_actions=info["legal_act"][np.newaxis])
+                    act = dist_pol.logits.argmax(-1).cpu().numpy()[0]
+
                 next_obs, rew, done, info = env.step(act)
 
                 black_turn = not black_turn
@@ -91,9 +100,15 @@ if __name__ == '__main__':
             })
         return epss
 
+    # Create folder
+    import os
+    name = str(size) + 'x' + str(size)
+    if not os.path.exists(f'/work/scratch/jb66zuhe/' + name):
+        os.makedirs(f'/work/scratch/jb66zuhe/' + name)
+
     # Sample and save
     eps = sample_eps(500)
-    with open(f'/work/scratch/jb66zuhe/eps_{config["job_id"]}.pkl', 'wb') as handle:
+    with open(f'/work/scratch/jb66zuhe/{name}/eps_{config["job_id"]}.pkl', 'wb') as handle:
         pickle.dump(eps, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     az.close()
